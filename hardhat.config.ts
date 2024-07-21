21bfe98f766c8025ec7f70d4d85a1fffb2ea03d2
Ishi181 <139246938+Ishi181@users.noreply.github.com>
2024-07-21 18:16:59 +0530

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,6 +10,7 @@ process.env.BASE_PATH = __dirname;
 
 // tasks
 import "./tasks/newcircuit.ts"
+import { network } from "hardhat";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -21,6 +22,13 @@ const config: HardhatUserConfig = {
         version: "0.6.11",
       }
     ]
+  },
+  networks: {
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/api_key",
+      chainId: 11155111,
+      accounts: ["private_key"]
+    }
   },
   circom: {
     // (optional) Base path for input files, defaults to `./circuits/`
